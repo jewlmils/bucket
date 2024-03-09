b9fae524a4dff445da8e10e0b174bc06e3f3c4ab
jewlmils <st.jewelmilagrosa@gmail.com>
2024-03-09 22:45:54 +0800

    fix: improve ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :categories do
-    resources :tasks
+    resources :tasks, except: [:index]
   end
   devise_for :users 
-  root 'home#index'
+  root 'categories#index'
 
   get '*path', to: redirect('/categories', status: 302)
 end
